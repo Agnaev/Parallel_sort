69fedc43466a71f58d36e3c442f89a3005b1c072
agnaev <agnev@mwpost.ru>
2019-10-09 23:32:17 +0300

    Сортировка массива в двух потоках

--- Parallel_processing.cpp
+++ Parallel_processing.cpp
@@ -3,85 +3,101 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <thread>
+#include <algorithm>
 
 using namespace std;
 
-
-class Sorting {
-public:
-	Sorting()
+vector<int> Sort(vector<int>& list) {
+	try
 	{
-
-	}
-	~Sorting() {
-
-	}
-	static vector<int> Sort(vector<int>& list) {
-		try
+		if (list.size() == 2 && list[0] > list[1])
 		{
-			if (list.size() == 2 && list[0] > list[1])
-				return swap(list, 0, 1);
-
-			if (list.size() == 2 || list.size() == 1 || list.size() == 0)
-				return list;
-
-			int middle = list.size() / 2;
-			int pivot = list[middle];
-			vector<int> firstHalf, secondHalf;
-			for (int i = 0; i < list.size(); i++)
-			{
-				if (i != list.size() / 2)
-				{
-					if (list[i] < pivot)
-					{
-						firstHalf.push_back(list[i]);
-					}
-					else if (list[i] >= pivot)
-					{
-						secondHalf.push_back(list[i]);
-					}
-				}
-			}
-
-			Sort(firstHalf);
-			Sort(secondHalf);
-
-			list.clear();
-
-			for (auto& elem : firstHalf) {
-				list.push_back(elem);
-			}
-			list.push_back(pivot);
-			for (auto& elem : secondHalf) {
-				list.push_back(elem);
-			}
+			int tmp = list[0];
+			list[1] = list[0];
+			list[0] = tmp;
 			return list;
 		}
-		catch (...) {
-			cout << "Error while sorting." << endl;
+
+		if (list.size() == 2 || list.size() == 1 || list.size() == 0)
+			return list;
+
+		int middle = list.size() / 2;
+		int pivot = list[middle];
+		vector<int> firstHalf, secondHalf;
+		for (int i = 0; i < list.size(); i++)
+		{
+			if (i != list.size() / 2)
+			{
+				if (list[i] < pivot)
+				{
+					firstHalf.push_back(list[i]);
+				}
+				else if (list[i] >= pivot)
+				{
+					secondHalf.push_back(list[i]);
+				}
+			}
+		}
+
+		Sort(firstHalf);
+		Sort(secondHalf);
+
+		list.clear();
+
+		for (auto& elem : firstHalf) {
+			list.push_back(elem);
+		}
+		list.push_back(pivot);
+		for (auto& elem : secondHalf) {
+			list.push_back(elem);
+		}
+		return list;
+	}
+	catch (...) {
+		cout << "Error while sorting." << endl;
+	}
+}
+
+void Run() {
+	vector<int> first_half, second_half, list = { 2, 4, 1, 6, 7, 4, 1, 10, 7, 6, 14, 19, 9 };
+
+	for (auto elem : list) {
+		if (elem < list[list.size() / 2]) {
+			first_half.push_back(elem);
+		}
+		else {
+			second_half.push_back(elem);
 		}
 	}
-private:
-	static vector<int> swap(vector<int> mas, int i, int j) {
-		int tmp = mas[0];
-		mas[1] = mas[0];
-		mas[0] = tmp;
-		return mas;
+
+	thread th(Sort, ref(first_half));
+	Sort(second_half);
+
+	if (th.joinable()) {
+		th.join();
 	}
 
-};
+	list.clear();
+
+	/*memcpy(&list, &first_half, first_half.size());
+	memcpy(&list, &second_half, second_half.size());*/
+
+	for (auto elem : first_half) {
+		list.push_back(elem);
+	}
+	for (auto elem : second_half) {
+		list.push_back(elem);
+	}
+
+	for (auto elem : list) {
+		printf("%d ", elem);
+	}
+	printf("\n");
+}
 
 int main()
 {
-	vector<int> list = {2, 4, 1, 6, 7, 4, 1, 10, 7, 6, 14, 19, 9};
-	/*list = Sort<int>(list);
-	for (int i : list) {
-		printf("%d ", i);
-	}*/
-	Sorting* sortPtr = new Sorting();
-	std::thread th(&Sorting::Sort, sortPtr, list);
-	th.join();
+	Run();
 	system("pause");
 }
