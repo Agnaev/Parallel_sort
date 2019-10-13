25b010176f3058cbf4dc312611baaa004590a3f2
agnaev <agnev@mwpost.ru>
2019-10-13 10:45:26 +0300

    update

--- Parallel_processing.cpp
+++ Parallel_processing.cpp
@@ -1,58 +1,52 @@
-﻿// Parallel_processing.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <thread>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> Sort(vector<int>& list) {
+void swap(vector<int> list, int a, int b) {
+	int tmp = list[a];
+	list[a] = list[b];
+	list[b] = list[a];
+}
+
+void Sort(vector<int>& list) {
 	try
 	{
 		if (list.size() == 2 && list[0] > list[1])
 		{
-			int tmp = list[0];
-			list[1] = list[0];
-			list[0] = tmp;
-			return list;
+			swap(list, 0, 1);
+			return;
 		}
 
 		if (list.size() == 2 || list.size() == 1 || list.size() == 0)
-			return list;
+			return;
 
 		int middle = list.size() / 2;
 		int pivot = list[middle];
 		vector<int> firstHalf, secondHalf;
 		for (int i = 0; i < list.size(); i++)
-		{
 			if (i != list.size() / 2)
-			{
+			
 				if (list[i] < pivot)
-				{
 					firstHalf.push_back(list[i]);
-				}
 				else if (list[i] >= pivot)
-				{
 					secondHalf.push_back(list[i]);
-				}
-			}
-		}
+			
 
 		Sort(firstHalf);
 		Sort(secondHalf);
 
 		list.clear();
 
-		for (auto& elem : firstHalf) {
+		for (auto& elem : firstHalf) 
 			list.push_back(elem);
-		}
 		list.push_back(pivot);
-		for (auto& elem : secondHalf) {
+		for (auto& elem : secondHalf) 
 			list.push_back(elem);
-		}
-		return list;
+		
+		//return list;
 	}
 	catch (...) {
 		cout << "Error while sorting." << endl;
@@ -60,44 +54,42 @@ vector<int> Sort(vector<int>& list) {
 }
 
 void Run() {
-	vector<int> first_half, second_half, list = { 2, 4, 1, 6, 7, 4, 1, 10, 7, 6, 14, 19, 9 };
+	try {
+		vector<int> first_half, second_half, list = { 2, 4, 1, 6, 7, 4, 1, 10, 7, 6, 14, 19, 9 };
 
-	for (auto elem : list) {
-		if (elem < list[list.size() / 2]) {
-			first_half.push_back(elem);
-		}
-		else {
-			second_half.push_back(elem);
-		}
+		for (auto elem : list) 
+			if (elem < list[list.size() / 2]) 
+				first_half.push_back(elem);
+			else 
+				second_half.push_back(elem);
+
+		thread th(Sort, ref(second_half));
+		Sort(first_half);
+
+		list.clear();
+
+		for (auto elem : first_half) 
+			list.push_back(elem);
+
+		if (th.joinable())  //wait
+			th.join();
+
+		for (auto elem : second_half) 
+			list.push_back(elem);
+
+		for (auto elem : list) 
+			printf("%d ", elem);
+		
+		printf("\n");
 	}
-
-	thread th(Sort, ref(first_half));
-	Sort(second_half);
-
-	if (th.joinable()) {
-		th.join();
+	catch (...) {
+		printf("Error while runing.");
 	}
-
-	list.clear();
-
-	/*memcpy(&list, &first_half, first_half.size());
-	memcpy(&list, &second_half, second_half.size());*/
-
-	for (auto elem : first_half) {
-		list.push_back(elem);
-	}
-	for (auto elem : second_half) {
-		list.push_back(elem);
-	}
-
-	for (auto elem : list) {
-		printf("%d ", elem);
-	}
-	printf("\n");
 }
 
 int main()
 {
 	Run();
 	system("pause");
+	return 0;
 }
