9f77076e39ea519c2af9ab41c4d1ea51eb482c75
agnaev <agnev@mwpost.ru>
2019-11-07 23:20:20 +0300

    code review

--- Parallel_processing.cpp
+++ Parallel_processing.cpp
@@ -3,23 +3,21 @@
 #include <thread>
 #include <algorithm>
 
-using namespace std;
-
 template<class T>
-void swap(vector<T> &list, int a, int b) {
+void swap(std::vector<T> &list, int a, int b) {
 	int tmp = list[a];
 	list[a] = list[b];
 	list[b] = list[a];
 }
 
 template<typename T>
-void AddRange(vector<T> &from, vector<T> &to) {
+void AddRange(std::vector<T> &from, std::vector<T> &to) {
 	for (T& elem : from) {
 		to.push_back(elem);
 	}
 }
 
-void Sort(vector<int>& list) {
+void Sort(std::vector<int>& list) {
 	try
 	{
 		if (list.size() == 2 && list[0] > list[1])
@@ -28,20 +26,23 @@ void Sort(vector<int>& list) {
 			return;
 		}
 
-		if (list.size() == 2 || list.size() == 1 || list.size() == 0)
+		if (list.size() == 2 || list.size() == 1 || list.size() == 0) {
 			return;
+		}
 
 		int middle = list.size() / 2;
 		int pivot = list[middle];
-		vector<int> firstHalf, secondHalf;
-		for (int i = 0; i < list.size(); i++)
-			if (i != list.size() / 2)
-			
-				if (list[i] < pivot)
+		std::vector<int> firstHalf, secondHalf;
+		for (int i = 0; i < list.size(); i++) {
+			if (i != list.size() / 2) {
+				if (list[i] < pivot) {
 					firstHalf.push_back(list[i]);
-				else if (list[i] >= pivot)
+				}
+				else if (list[i] >= pivot) {
 					secondHalf.push_back(list[i]);
-			
+				}
+			}
+		}
 
 		Sort(firstHalf);
 		Sort(secondHalf);
@@ -53,34 +54,38 @@ void Sort(vector<int>& list) {
 		AddRange(secondHalf, list);
 	}
 	catch (...) {
-		cout << "Error while sorting." << endl;
+		printf("Error while sorting.\n");
 	}
 }
 
 void Run() {
 	try {
-		vector<int> first_half, second_half, list = { 2, 4, 1, 6, 7, 4, 8, 10, 7, 6, 14, 19, 9 };
+		std::vector<int> first_half, second_half, list = { 2, 4, 1, 6, 7, 4, 8, 10, 7, 6, 14, 19, 9 };
 
-		for (auto elem : list) 
-			if (elem < list[list.size() / 2]) 
+		for (auto elem : list) {
+			if (elem < list[list.size() / 2]) {
 				first_half.push_back(elem);
-			else 
+			}
+			else {
 				second_half.push_back(elem);
-
-		thread th(Sort, ref(second_half));
+			}
+		}
+		std::thread th(Sort, ref(second_half));
 		Sort(first_half);
 
 		list.clear();
 
 		AddRange(first_half, list);
 
-		if (th.joinable())  //wait
+		if (th.joinable()) {  //wait
 			th.join();
+		}
 
 		AddRange(second_half, list);
 
-		for (auto elem : list) 
+		for (auto elem : list) {
 			printf("%d ", elem);
+		}
 		
 		printf("\n");
 	}
